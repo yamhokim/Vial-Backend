6aa0024b13c990be7355454e224392d0ef69de81
yamhokim <yoonho.luke.kim@gmail.com>
2025-05-07 01:31:24 -0400

    registered the /query route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify'
 
 import formDataRoutes from './routes/form_data'
+import queryRoutes from './routes/query'
 import errorHandler from './errors'
 
 function build(opts = {}) {
   const app = fastify(opts)
 
   app.register(formDataRoutes, { prefix: '/form-data' })
+  app.register(queryRoutes, { prefix: '/query' })
 
   app.setErrorHandler(errorHandler)
 
